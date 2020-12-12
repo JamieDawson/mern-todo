954163be0bd665898e880f8b3b2adb6c2174e7ba
JamieDawson <jamierobertdawson@gmail.com>
2020-12-12 15:38:12 -0800

    Now displays info added to DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react'; //useEffect: Call when rednered.
 import Axios from 'axios';
 
 function App() {
 	const [name, setName] = useState('');
 	const [age, setAge] = useState(0);
+	const [listOfFriends, setListOfFriends] = useState([]);
 
 	//name: name = name of variable - property [name, setName]
 	const addFriend = () => {
-		Axios.post('http://localhost:3001/addFriend', {name: name, age: age})
-			.then(() => {
-				alert('worked!');
+		Axios.post('http://localhost:3001/addFriend', {name: name, age: age});
+	};
+
+	useEffect(() => {
+		Axios.get('http://localhost:3001/read')
+			.then((response) => {
+				setListOfFriends(response.data);
 			})
 			.catch(() => {
-				alert('failed');
+				console.log('ERROR');
 			});
-	};
-	//test
+	}, []);
+
 	return (
 		<div className='App'>
 			<div className='inputs'>
@@ -36,6 +41,13 @@ function App() {
 				></input>
 				<button onClick={addFriend}>Add Friend</button>
 			</div>
+			{listOfFriends.map((val) => {
+				return (
+					<div>
+						{val.name} {val.age}
+					</div>
+				);
+			})}
 		</div>
 	);
 }
